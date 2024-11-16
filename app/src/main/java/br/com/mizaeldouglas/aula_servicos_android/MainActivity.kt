d299536b9713a60e6d476fe5e74d0e2e2614dd7e
MizaelDouglasDeMello <mizaeldouglas4@gmail.com>
2024-11-16 10:41:45 -0300

    criando meus serviços com Thread

--- app/src/main/java/br/com/mizaeldouglas/aula_servicos_android/MainActivity.kt
+++ app/src/main/java/br/com/mizaeldouglas/aula_servicos_android/MainActivity.kt
@@ -1,20 +1,33 @@
 package br.com.mizaeldouglas.aula_servicos_android
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import br.com.mizaeldouglas.aula_servicos_android.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val intentService = Intent(this, MeuService::class.java)
+
+        binding.btnIniciarService.setOnClickListener {
+            startService(intentService)
+//            startForegroundService()
         }
     }
 }
--- app/src/main/java/br/com/mizaeldouglas/aula_servicos_android/MeuService.kt
+++ app/src/main/java/br/com/mizaeldouglas/aula_servicos_android/MeuService.kt
@@ -3,9 +3,40 @@ package br.com.mizaeldouglas.aula_servicos_android
 import android.app.Service
 import android.content.Intent
 import android.os.IBinder
+import android.util.Log
 
-class MeuService: Service() {
+class MeuService : Service() {
     override fun onBind(intent: Intent?): IBinder? {
         return null
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        Log.i("Service", "onCreate ")
+
+    }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        val myThread = MyThread()
+        myThread.start()
+        return super.onStartCommand(intent, flags, startId)
+    }
+
+    inner class MyThread : Thread() {
+        override fun run() {
+            super.run()
+            for (i in 0..10) {
+                Log.i("Service", "Contador: $i")
+                sleep(1000)
+                Log.i("Service", "run: MyThread")
+            }
+            stopSelf()
+        }
+    }
+
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.i("Service", "onDestroy")
     }
 }
